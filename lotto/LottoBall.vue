ebcfdfda4ec1664ab4e34a335401e32ea25e0857
mirae <mirae611@nate.com>
2021-06-11 18:17:59 +0900

    lotto game

--- lotto/LottoBall.vue
+++ lotto/LottoBall.vue
@@ -1,15 +1,47 @@
 <template>
-  
+  <div class="ball" :style="styleObject">{{ number }}</div>
 </template>
 
 <script>
 export default {
+  name: "LottoBall",
+  props: {
+    number: Number,
+  },
   data() {
     return {}
+  },
+  computed: {    
+    styleObject() {
+    let background;
+      if (this.number <= 10) {
+        background = 'red';
+      } else if (this.number <= 20) {
+        background = 'orange';
+      } else if (this.number <= 30) {
+        background = 'yellow';
+      } else if (this.number <= 40) {
+        background = 'blue';
+      } else {
+        background = 'green';
+      }
+      return {
+        background,
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-
+.ball {
+  display: inline-block;
+  border: 1px solid #000;
+  border-radius: 50%;
+  width: 40px;
+  margin-right: 20px;
+  font-size: 20px;
+  line-height: 40px;
+  text-align: center;
+}
 </style>
--- lotto/LottoGenerator.vue
+++ lotto/LottoGenerator.vue
@@ -2,22 +2,62 @@
   <div>
     <p>당첨 숫자</p>
     <div id="result">
-      <lotto-ball v-for="(ball, i) in winBalls" :key="i"></lotto-ball>
+      <lotto-ball v-for="(ball, i) in winBalls" :key="i" :number="ball"></lotto-ball>
     </div>
     <p>보너스!</p>
-    <lotto-ball v-if="bonus"></lotto-ball>
-    <button v-if="redo">한 번 더!</button>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="redo" @click="onClickRedo">한 번 더!</button>
   </div>
 </template>
 
 <script>
+import LottoBall from './LottoBall.vue';
+
+function getWinNumbers() {
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() *  candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];  
+}
+
 export default {
+	components: {
+    LottoBall
+  },
   data() {
     return {
+      winNumbers: getWinNumbers(),
       winBalls: [],
       bonus: null,
       redo: false,
     }
+  },
+  methods: {
+    onClickRedo() {
+      this.winNumbers = getWinNumbers();
+      this.winBalls = [];
+      this.bonus = null;
+      this.redo = false;
+      this.showBalls();
+    },
+    showBalls() {      
+      for (let i = 0; i < this.winNumbers.length - 1; i++) {
+        setTimeout(() => {
+          this.winBalls.push(this.winNumbers[i]);
+        }, (i + 1) * 1000);
+      }
+      setTimeout(() => {
+        this.bonus = this.winNumbers[6];
+        this.redo = true;
+      }, 7000);
+    }
+  },
+  mounted() {
+    this.showBalls();
   }
 }
 </script>
