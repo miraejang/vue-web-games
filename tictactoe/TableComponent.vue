949ce95139d1d40d79b1735742a87bc374c1e8e0
mirae <mirae611@nate.com>
2021-06-17 14:00:43 +0900

    slot 추가

--- tictactoe/TableComponent.vue
+++ tictactoe/TableComponent.vue
@@ -1,25 +1,12 @@
 <template>
   <table>
-    <tr-component v-for="(rowData, index) in tableData" :row-index="index" :key="index"></tr-component>
+    <slot></slot>
   </table>
 </template>
 
 <script>
-import TrComponent from "./TrComponent";
-
 export default {
-  components: {
-    TrComponent,
-  },
-  // props: { tableData: Array }
-  computed: {
-    tableData() {
-      return this.$store.state.tableData;
-    },
-    turnMessage() {
-      return this.$store.getters.turnMessage;
-    }
-  }
+  
 }
 </script>
 
--- tictactoe/TicTacToe.vue
+++ tictactoe/TicTacToe.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <p>{{turn}}님의 턴입니다.</p>
-    <table-component :table-data="tableData"></table-component>
+    <table-component>
+      <tr v-for="(rowData, rowIndex) in tableData" :key="rowIndex">
+        <td @click="onClickTd(rowIndex, cellIndex)" v-for="(cellData, cellIndex) in rowData" :key="cellIndex">{{cellData}}</td>
+      </tr>
+    </table-component>
     <p v-if="winner">{{winner}}님의 승리!</p>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import store from "./store";
-import TableComponent from './TableComponent';
+import store, { SET_WINNER, CLICK_CELL, CHANGE_TURN, RESET_GAME, NO_WINNER } from "./store";
+import TableComponent from "./TableComponent";
 
 export default {
   store,
@@ -18,6 +22,47 @@ export default {
   },
   computed: {
     ...mapState(['tableData', 'winner', 'turn']),
+  },
+  methods: {
+    onClickTd(rowIndex, cellIndex) {
+      if (this.cellData) return;
+
+      let win = false;
+      this.$store.commit(CLICK_CELL, { row: rowIndex, cell: cellIndex });
+
+      if (this.tableData[rowIndex][0] === this.turn && this.tableData[rowIndex][1] === this.turn && this.tableData[rowIndex][2] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][cellIndex] === this.turn && this.tableData[1][cellIndex] === this.turn && this.tableData[2][cellIndex] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn) {
+        win = true;
+      }
+
+      if (win) { // 이긴 경우 : 3줄 달성
+        this.$store.commit(SET_WINNER, this.turn);
+        this.$store.commit(RESET_GAME);
+      } else { //  무승부
+        let all = true;
+        this.tableData.forEach(row => { // 무승부 검사
+          row.forEach(cell => {
+            if(!cell) {
+              all = false;
+            }
+          });
+        });
+        if (all) { // 무승부
+          this.$store.commit(NO_WINNER);
+          this.$store.commit(RESET_GAME);
+        } else { // 아직 게임이 끝나지 않은 경우
+          this.$store.commit(CHANGE_TURN);
+        }
+      }
+    }
   }
 }
 </script>
